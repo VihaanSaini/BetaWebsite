6656393b7e8869794b8bd7585be562076f3c1b24
Vihaan <87207836+VihaanSaini@users.noreply.github.com>
2022-05-21 20:58:36 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,23 @@
+// Site Settings //
+
+var siteTitle = `Vihaan Saini`;
+var siteDescription = "This Is The Officle Website For Vhaan Saini, This Website Is Still In Work's But It Will Up Runing In No Time Most likly By End Of July Thank You";
+var buttonColor = "#FFF000";
+var buttonHoverColor = "#1d1d1d";
+
+var firstBoxGradColor = "#00F0F8F"; //FFDB25 00F0F8F
+var secondBoxGradColor = "#00F0F80F"; //FA8C00 00F0F80F
+
+// Buttons //
+// Isons can range from anything in /assets/icons.svg. Like youtube, discord, twitter, github, globe etc.
+var buttonsArray = [
+    // Format: "LINK::Icon"
+    // "http://example.com::twitter"
+    "https://vihaansaini.github.io/::globe",
+    "https://discord.gg/PCypEXv5Wa::discord",
+    "https://www.youtube.com/channel/UCGieKr5OZNYJkX-ApseKSuA::youtube",
+    "https://instagram.com/vihaan_saini/::instagram",     
+    "https://patreon.com/vihaansaini/::patreon", 
+    "https://github.com/VihaanSaini::github",
+//    "https://vihaansaini00@gmail.com::email",
+]
