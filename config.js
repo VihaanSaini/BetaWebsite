4a9b85925ac46592cef5c2ffb988d82dbd3047aa
Vihaan <87207836+VihaanSaini@users.noreply.github.com>
2022-05-22 18:47:03 -0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 var siteTitle = `Vihaan Saini`;
 var siteDescription = "This Is The Officle Website For Vhaan Saini, This Website Is Still In Work's But It Will Up Runing In No Time Most likly By End Of July Thank You";
 var buttonColor = "#FFF000";
-var buttonHoverColor = "#1d1d1d";
+var buttonHoverColor = "#89CFF0";
 
 var firstBoxGradColor = "#00F0F8F"; //FFDB25 00F0F8F
 var secondBoxGradColor = "#00F0F80F"; //FA8C00 00F0F80F
